c8df3b363ac79a89f112258e5a74fbbf372d1099
Deepankar Rao <70944774+DeepankarRaoOyo@users.noreply.github.com>
2021-09-06 10:08:58 +0530

    some print statement added.

--- pyspark_demo.py
+++ pyspark_demo.py
@@ -6,19 +6,25 @@ spark = SparkSession.builder.appName('SparkByExamples.com').getOrCreate()
 dept = [("Finance",10),("Marketing",20),("Sales",30),("IT",40)]
 rdd = spark.sparkContext.parallelize(dept)
 
+print("first print")
 df = rdd.toDF()
 df.printSchema()
 df.show(truncate=False)
+print("-------------------")
 
+print("2nd print")
 deptColumns = ["dept_name","dept_id"]
 df2 = rdd.toDF(deptColumns)
 df2.printSchema()
 df2.show(truncate=False)
+print("-------------------")
 
 deptDF = spark.createDataFrame(rdd, schema = deptColumns)
 deptDF.printSchema()
 deptDF.show(truncate=False)
+print("-------------------")
 
+print("3rd print")
 from pyspark.sql.types import StructType,StructField, StringType
 deptSchema = StructType([       
     StructField('dept_name', StringType(), True),
